c15e32b168a66f726c36f98c9778ab49a646f154
Bryam11 <xavierchuchuca18@gmail.com>
2022-06-14 14:20:04 -0500

    learning React

---
+++ src/pages/SetAvatar.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {  useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { registerRoute } from "../utils/APIRoute";
+
+export default function SetAvatar() {
+  return (
+    <div>SetAvatar</div>
+  )
+}
---
+++ src/utils/APIRoute.js
@@ -0,0 +1,4 @@
+const host = "http://localhost:3001";
+const host1 = "https://chat-brhyant.onrender.com";
+export const registerRoute = `${host}/api/auth/register`;
+export const loginRoute = `${host}/api/auth/login`;
