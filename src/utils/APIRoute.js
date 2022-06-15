c0b1fb344afecc671f4a6a9bdc06cfb7c88fde2f
Bryam11 <xavierchuchuca18@gmail.com>
2022-06-15 17:58:49 -0500

    Change of configuration that prodcution

--- src/utils/APIRoute.js
+++ src/utils/APIRoute.js
@@ -1,8 +1,8 @@
-export const host = "http://localhost:3001";
+//export const host = "http://localhost:3001";
 export const host1 = "https://chat-brhyant.onrender.com";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const getAllMessageRoute = `${host}/api/messages/getmsg`;
+export const registerRoute = `${host1}/api/auth/register`;
+export const loginRoute = `${host1}/api/auth/login`;
+export const setAvatarRoute = `${host1}/api/auth/setavatar`;
+export const allUsersRoute = `${host1}/api/auth/allusers`;
+export const sendMessageRoute = `${host1}/api/messages/addmsg`;
+export const getAllMessageRoute = `${host1}/api/messages/getmsg`;
